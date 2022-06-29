707180be01b8f1d345add39aa19a228cdb062fda
Anouar9172 <94382980+Anouar9172@users.noreply.github.com>
2022-06-29 10:07:13 +0200

    1

---
+++ nieuwe infuze/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>infuze</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <title>infuze</title>
+    <title></title>
+  </head>
+  <body>
+
+
+  </body>
+</html>
